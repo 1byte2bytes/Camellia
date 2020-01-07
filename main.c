fd0f636c56a7789e1c4ef87ca2ffd6dbf5fbfac7
Anne <sydneyerickson@icloud.com>
2020-01-06 21:56:02 -0800

    Attempt to fix init issues

--- main.c
+++ main.c
@@ -4,24 +4,13 @@
 #include "stdio.h"
 #include "stdint.h"
 
-typedef struct {
-    char *name;
-    void (*init_func)();
-} init_func_t;
-
-init_func_t early_init[] = {
-#ifdef SERIAL_ENABLED
-        {"Serial", serial_init},
-#endif
-#ifdef VGA_ENABLED
-        {"VGA", vga_initialize}
-#endif
-};
-
 void kernel_main() {
     // Init early modules
-    for (size_t i = 0; i < sizeof(early_init); i++) {
-        early_init[i].init_func();
-    }
+#ifdef SERIAL_ENABLED
+    serial_init();
+#endif
+#ifdef VGA_ENABLED
+    vga_initialize();
+#endif
     kprintf("Camellia init");
 }
---
+++ qemu.bat
@@ -0,0 +1,1 @@
+qemu-system-x86_64 -kernel build\kernel.sys -machine type=pc-i440fx-3.1 -serial stdio
